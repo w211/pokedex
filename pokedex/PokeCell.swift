e6070ade633f63bc2e5cc2034683e681b69f3ad0
Marshall Cuico <marshallcuico@Marshalls-MacBook-Pro.local>
2016-03-16 23:09:30 -0700

    Added IBOutlets and PokeCell

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,25 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Marshall Cuico on 3/16/16.
+//  Copyright © 2016 Marshall Cuico. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
