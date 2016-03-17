ba0b4018fa7ba3b72a00987d0ed0cb19aeb0f34a
Marshall Cuico <marshallcuico@Marshalls-MacBook-Pro.local>
2016-03-16 20:00:17 -0700

    added collection view

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Marshall Cuico on 3/16/16.
+//  Copyright © 2016 Marshall Cuico. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
